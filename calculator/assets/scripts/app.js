7ff408e831642014d3c8918db4bb2296780e557c
Gihoon Lim <knv429@naver.com>
2024-04-23 16:03:28 +0900

    코드 분할 및 버튼 함수 연결

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -4,11 +4,40 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
-function add() {
-  const enteredNumber = getUserNumberInput();
-  const description = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const description = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, description);
 }
 
+function add() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
